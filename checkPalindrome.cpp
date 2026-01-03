68a43d01a0505bbf4cb67133878987063ec008cb
Ali Arman Jayed <aliarmanjayed1245@gmail.com>
2026-01-03 13:49:42 +0600

    3/1/2026 daily works

---
+++ checkPalindrome.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    bool isPalindrome = true;
+    for(int i = 0; i < n / 2; i++){
+        if(arr[i] != arr[n - i - 1]){
+            isPalindrome = false;
+            break;
+        }
+    }   
+    if(isPalindrome){
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
+}
